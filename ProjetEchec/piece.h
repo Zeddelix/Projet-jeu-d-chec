cf67b154a7fff2bc9077139d6829091034deac02
Killian <deveze.killian.6e@gmail.com>
2020-11-17 14:42:54 +0100

    V2
    Modifications diverses de la classe piece et réglement compatibilité du projet reçu

--- ProjetEchec/piece.h
+++ ProjetEchec/piece.h
@@ -1,25 +1,20 @@
 #ifndef PIECE_H
 #define PIECE_H
 
-#include <QMainWindow>
-#include "case.h"
+#include <string>
 
-QT_BEGIN_NAMESPACE
 namespace Ui { class Piece; }
-QT_END_NAMESPACE
 
-class Piece : public QMainWindow
+class Piece
 {
-    Q_OBJECT
-
 public:
-    Piece(QWidget *parent = nullptr);
+    Piece();
     virtual ~Piece();
     virtual bool PeutDeplacer(Plateau p,case dep, cas arr) = 0;
-    virtual QString nom() const;
+    virtual std::string nom() const;
     virtual void Deplacer(case arr) = 0;
 private:
     bool d_couleur;
-    QPixmap d_image;
+    std::string d_image;
 };
 #endif // PIECE_H
