22994db7ff28e9046eb87b93871e0407eb57219f
Killian <deveze.killian.6e@gmail.com>
2020-11-22 17:43:36 +0100

    Réglage du projet en c++14

--- ProjetEchec/ProjetEchec.pro
+++ ProjetEchec/ProjetEchec.pro
@@ -1,6 +1,6 @@
 QT -= gui
 
-CONFIG += c++11 console
+CONFIG += C++14 console
 CONFIG -= app_bundle
 
 # You can make your code fail to compile if it uses deprecated APIs.
--- ProjetEchec/main.cpp
+++ ProjetEchec/main.cpp
@@ -1,8 +1,18 @@
+/**
+cmake_minimum_required(VERSION 3.14.0)
+set(CMAKE_CXX_STANDARD 14)
+*/
+
 #include <QCoreApplication>
+
+#include <memory>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
     return a.exec();
+
+
+   auto p = std::make_unique<int>(5);
+
 }
