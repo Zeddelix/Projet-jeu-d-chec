8229670fe8ba4ea83ff922d1b8fcebb09e1d3139
Killian <deveze.killian.6e@gmail.com>
2020-12-18 16:46:21 +0100

    Ajout Piece  h
    Séparation de piece .h qui était dans le partie.h
    ajout de la méthode tabdeplacment possible

--- ProjetEchec/partie.h
+++ ProjetEchec/partie.h
@@ -1,30 +1,10 @@
 #ifndef PARTIE_H
 #define PARTIE_H
 
-#include <QObject>
-#include <QWidget>
 #include <QVector>
 #include <iostream>
-#include <memory>
 #include "piece.h"
 #include <vector>
-/** a suprimer*/
-class piece
-{
-public:
-    piece(const bool couleur,QPixmap image,QString nom);
-    virtual ~piece();
-    virtual bool peutDeplacer(const QVector<QVector<std::unique_ptr<piece>>> &p,const QPoint *dep, const QPoint *arr,const bool couleur) = 0;
-    QString nom() const;
-    bool couleur() const;
-    QPixmap *image() const;
-    QVector <QPoint> tabDeplacementPossible (const QVector<QVector<std::unique_ptr<piece>>> &p, QPoint *casDep);
-private:
-    QString d_nom;
-    bool d_couleur;
-    QPixmap d_image;
-    bool premierDeplacement;
-};
 
 class partie
 {
--- ProjetEchec/piece.h
+++ ProjetEchec/piece.h
@@ -1,6 +1,27 @@
 #ifndef PIECE_H
 #define PIECE_H
 
+#include <QObject>
+#include <QWidget>
+#include <memory>
 
+/** a suprimer*/
+class piece
+{
+public:
+    piece(const bool couleur,QPixmap image,QString nom);
+    virtual ~piece();
+    virtual bool peutDeplacer(const QVector<QVector<std::unique_ptr<piece>>> &p,const QPoint *dep, const QPoint *arr,const bool couleur) = 0;
+    QString nom() const;
+    bool couleur() const;
+    QPixmap *image() const;
+    QVector <QPoint> tabDeplacementPossible (const QVector<QVector<std::unique_ptr<piece>>> &p, QPoint *casDep);
+private:
+    QString d_nom;
+    bool d_couleur;
+    QPixmap d_image;
+    bool premierDeplacement;
+
+};
 
 #endif // PIECE_H
