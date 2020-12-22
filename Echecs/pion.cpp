b181acea261b94164236f16e7764eaa0f074cd8e
Gaulthier <g.petitdemange94@orange.fr>
2020-12-22 13:07:12 +0100

    Compilation OK pion

--- Echecs/pion.cpp
+++ Echecs/pion.cpp
@@ -11,7 +11,7 @@ Pion::Pion(const bool couleur,QPixmap image, const QString nom) : piece{couleur,
 
 bool Pion::peutDeplacer(const QVector<QVector<std::unique_ptr<piece>>> &p, const QPoint *depart, const QPoint *arrivee, const bool couleur)
 {
-    int x = arrivee->x()-depart->x();
+    int x = arrivee->x() - depart->x();
     int y = arrivee->y() - depart->y();
 
     if (couleur==0)//le pion est blanc, blanc en bas
@@ -22,11 +22,11 @@ bool Pion::peutDeplacer(const QVector<QVector<std::unique_ptr<piece>>> &p, const
             }
         if (x==0 && y==2 && depart->y()==1)
         {
-            if(p(arrivee->x(), arrivee->y()-1)==nullptr && p[arrivee->x()][arrivee->y()]==nullptr) return true; // les cases +1 et +2 sont libres)
+            if( p[arrivee->x()][arrivee->y()-1]==nullptr && p[arrivee->x()][arrivee->y()]==nullptr) return true; // les cases +1 et +2 sont libres)
         }
         if ((x==1 && y==1) || (x==-1 && y==1))
         {
-            if (p[arrivee->x()][arrivee->y()]->couleur==1) return true;//bouffer en diagonale
+            if ( p[arrivee->x()][arrivee->y()] -> couleur() ==1 ) return true;//bouffer en diagonale
         }
     }
 
@@ -39,17 +39,15 @@ bool Pion::peutDeplacer(const QVector<QVector<std::unique_ptr<piece>>> &p, const
             }
         if (x==0 && y==-2 && depart->y()==6)
         {
-            if(p(arrivee->x(), arrivee->y()+1)==nullptr && p[arrivee->x()][arrivee->y()]==nullptr) return true; // les cases -1 et -2 sont libres)
+            if(p[arrivee->x()][arrivee->y()+1]==nullptr && p[arrivee->x()][arrivee->y()]==nullptr) return true; // les cases -1 et -2 sont libres)
         }
         if ((x==1 && y==-1) || (x==-1 && y==-1))
         {
-            if (p[arrivee->x()][arrivee->y()]->couleur==0) return true; //bouffer en diagonale
+            if (p[arrivee->x()][arrivee->y()]->couleur()==0) return true; //bouffer en diagonale
         }
     }
     return false;
 
-     }
-
-
 }
+
 
